cda3f5604d5407add71dc35f8c22cfd183c20ccf
MentionSon <mentionlyze@outlook.com>
2024-10-24 18:01:54 +0800

    feat: modify materials

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,36 +1,42 @@
 import * as THREE from 'three'
 
 const defaultCameraConfig = {
-    fov: 75,
-    aspectRatio: window.innerWidth / window.innerHeight,
-    near: 0.1,
-    far: 1000
+  fov: 75,
+  aspectRatio: window.innerWidth / window.innerHeight,
+  near: 0.1,
+  far: 1000,
 }
 
 const defaultRendererConfig: THREE.WebGLRendererParameters = {
-
-    antialias: true,
+  antialias: true,
 }
 
-export function createThreeContext(dom: HTMLCanvasElement, cameraConfig: Record<string, any> = defaultCameraConfig, rendererConfig: THREE.WebGLRendererParameters = defaultRendererConfig) {
-    const scene = new THREE.Scene()
-    const camera = new THREE.PerspectiveCamera(cameraConfig.fov, cameraConfig.aspectRatio, cameraConfig.near, cameraConfig.far)
-    camera.position.z = 5
-    const renderer = new THREE.WebGLRenderer({ ...rendererConfig, canvas: dom })
+export function createThreeContext(
+  dom: HTMLCanvasElement,
+  cameraConfig: Record<string, any> = defaultCameraConfig,
+  rendererConfig: THREE.WebGLRendererParameters = defaultRendererConfig
+) {
+  const scene = new THREE.Scene()
+  const camera = new THREE.PerspectiveCamera(cameraConfig.fov, cameraConfig.aspectRatio, cameraConfig.near, cameraConfig.far)
+  camera.position.z = 5
+  const renderer = new THREE.WebGLRenderer({ ...rendererConfig, canvas: dom })
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  renderer.setPixelRatio(window.devicePixelRatio)
+
+  renderer.shadowMap.enabled = true
+  renderer.shadowMap.type = THREE.PCFShadowMap
+  renderer.outputColorSpace = THREE.SRGBColorSpace
+  renderer.toneMapping = THREE.ACESFilmicToneMapping
+  renderer.toneMappingExposure = 1
+
+  document.body.appendChild(renderer.domElement)
+
+  window.addEventListener('resize', () => {
+    console.log('resize')
     renderer.setSize(window.innerWidth, window.innerHeight)
-    renderer.setPixelRatio(window.devicePixelRatio)
+    camera.aspect = dom.clientWidth / dom.clientHeight
+    camera.updateProjectionMatrix()
+  })
 
-    renderer.shadowMap.enabled = true
-    renderer.shadowMap.type = THREE.PCFSoftShadowMap
-
-    document.body.appendChild(renderer.domElement)
-
-    window.addEventListener('resize', () => {
-        console.log('resize')
-        renderer.setSize(window.innerWidth, window.innerHeight)
-        camera.aspect = dom.clientWidth / dom.clientHeight
-        camera.updateProjectionMatrix()
-    })
-
-    return { scene, camera, renderer }
+  return { scene, camera, renderer }
 }
--- playground/base-samples/samples/21_modify_materials/src/index.ts
+++ playground/base-samples/samples/21_modify_materials/src/index.ts
@@ -14,6 +14,17 @@ const textureLoader = new THREE.TextureLoader()
 const gltfLoader = new GLTFLoader()
 const cubeTextureLoad = new THREE.CubeTextureLoader()
 
+const updateAllMaterial = () => {
+  scene.traverse((child) => {
+    if ((<THREE.Mesh>child).isMesh && (<THREE.Mesh>child).material instanceof THREE.MeshStandardMaterial) {
+      ;((<THREE.Mesh>child).material as THREE.MeshStandardMaterial).envMapIntensity = 1
+      ;((<THREE.Mesh>child).material as THREE.MeshStandardMaterial).needsUpdate = true
+      ;(<THREE.Mesh>child).castShadow = true
+      ;(<THREE.Mesh>child).receiveShadow = true
+    }
+  })
+}
+
 const environmentMap = cubeTextureLoad.load([
   './static/textures/environmentMaps/0/px.jpg',
   './static/textures/environmentMaps/0/nx.jpg',
@@ -37,16 +48,92 @@ const material = new THREE.MeshStandardMaterial({
   normalMap: normalTexture,
 })
 
+const depthMaterial = new THREE.MeshDepthMaterial({
+  depthPacking: THREE.RGBADepthPacking,
+})
+
+const customeUniforms = {
+  uTime: { value: 0 },
+}
+
 material.onBeforeCompile = (shader) => {
-  console.log(shader)
+  shader.uniforms.uTime = customeUniforms.uTime
+  shader.vertexShader = shader.vertexShader.replace(
+    '#include <common>',
+    `
+      #include <common>
+
+      uniform float uTime;
+
+      mat2 get2dRotateMatrix(float _angle) {
+        return mat2(cos(_angle), -sin(_angle), sin(_angle), cos(_angle));
+      }
+    `
+  )
+
+  shader.vertexShader = shader.vertexShader.replace(
+    '#include <beginnormal_vertex>',
+    `
+      #include <beginnormal_vertex> 
+      float angle = (sin(position.y) + uTime) * 0.9;
+      mat2 rotateMatrix = get2dRotateMatrix(angle);
+
+      objectNormal.xz = rotateMatrix * objectNormal.xz;
+    `
+  )
+
+  shader.vertexShader = shader.vertexShader.replace(
+    '#include <begin_vertex>',
+    `
+      #include <begin_vertex> 
+
+
+      transformed.xz = rotateMatrix * transformed.xz;
+    `
+  )
+}
+
+depthMaterial.onBeforeCompile = (shader) => {
+  shader.uniforms.uTime = customeUniforms.uTime
+  shader.vertexShader = shader.vertexShader.replace(
+    '#include <common>',
+    `
+      #include <common>
+
+      uniform float uTime;
+
+      mat2 get2dRotateMatrix(float _angle) {
+        return mat2(cos(_angle), -sin(_angle), sin(_angle), cos(_angle));
+      }
+    `
+  )
+  shader.vertexShader = shader.vertexShader.replace(
+    '#include <begin_vertex>',
+    `
+      #include <begin_vertex> 
+
+      float angle = (sin(position.y) + uTime) * 0.9;
+      mat2 rotateMatrix = get2dRotateMatrix(angle);
+      transformed.xz = rotateMatrix * transformed.xz;
+    `
+  )
 }
 
 gltfLoader.load('./static/models/LeePerrySmith/LeePerrySmith.glb', (gltf) => {
   const mesh = gltf.scene.children[0] as THREE.Mesh
   mesh.rotation.y = Math.PI * 0.5
   mesh.material = material
+  mesh.customDepthMaterial = depthMaterial
   scene.add(mesh)
+
+  updateAllMaterial()
 })
+
+const plane = new THREE.Mesh(new THREE.PlaneGeometry(15, 15, 15), new THREE.MeshStandardMaterial())
+plane.rotation.y = -Math.PI
+plane.position.y = -5
+plane.position.z = 5
+scene.add(plane)
 
 /**
  * Lights
@@ -56,8 +143,11 @@ directionalLight.castShadow = true
 directionalLight.shadow.mapSize.set(1024, 1024)
 directionalLight.shadow.camera.far = 15
 directionalLight.shadow.normalBias = 0.05
-directionalLight.position.set(0.25, 2, -2.25)
+directionalLight.position.set(0.5, 4, -4.5)
 scene.add(directionalLight)
+
+const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 1)
+scene.add(directionalLightHelper)
 
 const controls = new OrbitControls(camera, renderer.domElement)
 controls.enableDamping = true
@@ -69,6 +159,8 @@ function tick() {
   const elapsedTime = clock.getElapsedTime()
   const deltaTime = elapsedTime - lastFrameTime
   lastFrameTime = elapsedTime
+
+  customeUniforms.uTime.value = elapsedTime
 
   controls.update()
   renderer.render(scene, camera)
